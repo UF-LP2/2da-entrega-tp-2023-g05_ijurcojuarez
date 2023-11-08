6a60fb08ef44d4a60305462ea0c69b53b9de727b
jazmin <jazmin.ijurco@gmail.com>
2023-11-08 17:15:16 -0300

    arreglo de errores

--- src/Cronometro.py
+++ src/Cronometro.py
@@ -1,44 +1,63 @@
 from datetime import datetime
 import tkinter as tk
-INTERVALO_REFRESCO = 1000  # En milisegundos
 
-#hora_inicio = datetime.now()
-segundos_transcurridos=-2
+INTERVALO_REFRESCO = 1000  # En milisegundos (1 segundo real)
+
+hora_inicio = datetime.now()
+minutos_programa = 0
+
+def iniciar_reloj():
+    global hora_inicio, en_ejecucion
+    hora_inicio = datetime.now()
+    en_ejecucion = True
+    refrescar_tiempo_transcurrido()
+
+def detener_reloj():
+    global en_ejecucion
+    en_ejecucion = False
+
+def minutos_transcurridos():
+    segundos_transcurridos = (datetime.now() - hora_inicio).total_seconds()
+    minutos_programa = int(segundos_transcurridos)
+    return minutos_programa
 
 def segundos_a_segundos_minutos_y_horas(segundos):
-    horas = int(segundos / 15 / 5)
-    segundos -= horas * 15 * 5
-    minutos = int(segundos / 5)
-    segundos -= minutos * 5
-    return f"{horas:02d}:{minutos:02d}:{segundos:02d}"
-
-
-
-def reiniciar_cronometro():
-    global segundos_transcurridos
-    segundos_transcurridos = 0
+    horas = int(segundos / 60 / 60)
+    segundos -= horas * 60 * 60
+    minutos = int(segundos / 60)
+    segundos -= minutos * 60
+    return f"{horas:02d}:{minutos:02d}:{int(segundos):02d}"
 
 def obtener_tiempo_transcurrido_formateado():
-    global segundos_transcurridos
-    segundos_transcurridos += 1
-    if segundos_transcurridos == 24:  # Si han pasado 23 horas
-        reiniciar_cronometro()
-    return segundos_a_segundos_minutos_y_horas(segundos_transcurridos)
-
+    global minutos_programa
+    if en_ejecucion:
+        segundos_reales = (datetime.now() - hora_inicio).total_seconds()
+        minutos_programa = int(segundos_reales)  # Cada minuto en el programa avanza a medida que pasa 1 segundo real
+    horas_programa = minutos_programa / 60
+    return segundos_a_segundos_minutos_y_horas(int(horas_programa * 60 * 60))
 
 def refrescar_tiempo_transcurrido():
-    #print("Refrescando!")
-    variable_hora_actual.set(obtener_tiempo_transcurrido_formateado())
-    raiz.after(INTERVALO_REFRESCO, refrescar_tiempo_transcurrido)
-
+    if en_ejecucion:
+        variable_hora_actual.set(obtener_tiempo_transcurrido_formateado())
+        minutos_pasados_label.config(text=f"Minutos transcurridos: {minutos_transcurridos()}")
+        raiz.after(INTERVALO_REFRESCO, refrescar_tiempo_transcurrido)
 
 raiz = tk.Tk()
-variable_hora_actual = tk.StringVar(raiz, value=obtener_tiempo_transcurrido_formateado())
-raiz.etiqueta = tk.Label(
-    raiz, textvariable=variable_hora_actual, font=f"Consolas 60")
+variable_hora_actual = tk.StringVar(raiz, value="00:00:00")
+raiz.etiqueta = tk.Label(raiz, textvariable=variable_hora_actual, font="Consolas 60")
 raiz.etiqueta.pack(side="top")
+
+boton_iniciar = tk.Button(raiz, text="Iniciar", command=iniciar_reloj)
+boton_iniciar.pack()
+
+boton_detener = tk.Button(raiz, text="Detener", command=detener_reloj)
+boton_detener.pack()
+
+minutos_pasados_label = tk.Label(raiz, text="Minutos transcurridos: 0")
+minutos_pasados_label.pack()
+
 app = tk.Frame()
-raiz.title("Cronómetro con Tkinter - By Parzibyte")
-refrescar_tiempo_transcurrido()
+raiz.title("reloj")
+
 app.pack()
-app.mainloop()
+raiz.mainloop()
--- src/Triage.py
+++ src/Triage.py
@@ -1,93 +1,101 @@
-from Cronometro import obtener_tiempo_transcurrido_formateado
+from Cronometro import minutos_transcurridos
 
 enfermeros: int
-pacientes = [ ]
-medicos: int    #infinitos
+pacientes = []
+medicos: int    # infinitos
 
-def AsignarEnfermeros():
-    if obtener_tiempo_transcurrido_formateado() == 23:
+
+def asignar_enfermeros():
+    if minutos_transcurridos() == 1380:  # minutos de 23 horas
         enfermeros = 1
 
-    if obtener_tiempo_transcurrido_formateado() == 6:
+    if minutos_transcurridos() == 360:  # minutos de 6 horas
         enfermeros = 2
 
-    if obtener_tiempo_transcurrido_formateado() == 10:
+    if minutos_transcurridos() == 600:  # minutos de 10 horas
         enfermeros = 5
 
-    if obtener_tiempo_transcurrido_formateado() == 16:
+    if minutos_transcurridos() == 960:  # minutos de 16 horas
         enfermeros = 3
 
 
-def AsignarGravedad(pacientes):
+def asignar_gravedad(pacientes):
     for i in range(len(pacientes)):
-        if pacientes[i].síntomas == 'politraumatismo grave':
-            pacientes.gravedad= 5    # rojo
+        if pacientes[i]["síntomas"] == 'politraumatismo grave':
+            pacientes["gravedad"] = 5    # rojo
 
-        elif pacientes[i].síntomas  == 'coma' or pacientes[i].síntomas== 'convulsiones' or pacientes[i].síntomas == 'hemorragia digestiva' or pacientes[i].síntomas == 'isquemia':
-            pacientes[i].gravedad= 4     #naranja
+        elif pacientes[i]["síntomas"] == 'coma' or pacientes[i]["síntomas"] == 'convulsiones' or pacientes[i]["síntomas"] == 'hemorragia digestiva' or pacientes[i]["síntomas"] == 'isquemia':
+            pacientes[i]["gravedad"] = 4     # naranja
 
-        elif pacientes[i].síntomas == 'cefalea brusca' or pacientes[i].síntomas == 'paresia' or pacientes[i].síntomas == 'hipertension arterial' or pacientes[i].síntomas == 'vertigo con afectacion vegetativa'or pacientes[i].síntomas == 'sincope'or pacientes[i].síntomas == 'urgencias psiquiatricas':
-            pacientes[i].gravedad = 3     #amarilla
+        elif pacientes[i]["síntomas"] == 'cefalea brusca' or pacientes[i]["síntomas"] == 'paresia' or pacientes[i]["síntomas"] == 'hipertension arterial' or pacientes[i]["síntomas"] == 'vertigo con afectacion vegetativa' or pacientes[i]["síntomas"] == 'sincope' or pacientes[i]["síntomas"] == 'urgencias psiquiatricas':
+            pacientes[i]["gravedad"] = 3     # amarilla
 
+        elif pacientes[i]["síntomas"] == 'otalgias' or pacientes[i]["síntomas"] == 'odontalgias' or pacientes[i]["síntomas"] == 'dolores inespecificos leves' or pacientes[i]["síntomas"] == 'traumatismos' or pacientes[i]["síntomas"] == 'esguinces' :
+            pacientes[i]["gravedad"] = 2      # verde
 
-        elif pacientes[i].síntomas == 'otalgias' or pacientes[i].síntomas == 'odontalgias' or pacientes[i].síntomas == 'dolores inespecificos leves' or pacientes[i].síntomas == 'traumatismos'or pacientes[i].síntomas == 'esguinces':
-            pacientes[i].gravedad= 2      #verde
+        elif pacientes[i]["síntomas"] == 'cefalea' or pacientes[i]["síntomas"]== 'tos':
+            pacientes[i]["gravedad"] = 1      # azul
 
-
-        elif pacientes[i].síntomas == 'cefalea' or pacientes[i].síntomas == 'tos':
-            pacientes[i].gravedad= 1      #azul
-
-        pacientes[i].tiempo_espera = obtener_tiempo_transcurrido_formateado()     #asigno el tiempo en el cual fue revisado
+        pacientes[i]["tiempo_espera"] = minutos_transcurridos()     # asigno el tiempo en el cual fue revisado
 
 
-
-def CambiarGravedad(pacientes):
+def cambiar_gravedad(pacientes):
+    # chequear color y si su tiempo es mayor al tiempo de espera del siguiente cambiar color
     for i in range(len(pacientes)):
-      if obtener_tiempo_transcurrido_formateado()  > pacientes[i].tiempo_espera:
-        pacientes[i].gravedad= 5 #cuando ya pasó su tiempo se lo atiende inmediatamente
+        if pacientes[i]["gravedad"] == 1 and pacientes[i]["tiempo_espera"] >= 120:  # llego al tiempo del verde -> lo cambio
+            pacientes[i]["gravedad"] = 2
+
+        if pacientes[i]["gravedad"] == 2 and pacientes[i]["tiempo_espera"] >= 60:  # llego al tiempo del amarillo -> lo cambio
+            pacientes[i]["gravedad"] = 3
+
+        if pacientes[i]["gravedad"] == 3 and pacientes[i]["tiempo_espera"] >= 50:  # llego al tiempo del naranja -> lo cambio
+            pacientes[i]["gravedad"] = 4
+
+        if pacientes[i]["gravedad"] == 4 and pacientes[i]["tiempo_espera"] >= 9:  # llego al tiempo del rojo -> lo cambio
+            pacientes[i]["gravedad"] = 5
 
 
-def AtenderPaciente(dni):
-    pos=pacientes.index(pacientes.dni)  #index busca la posicion en la lista
-    del pacientes[pos]   #borra al paciente, se va del hospital
+def atender_paciente(dni):
+    pos = pacientes.index(pacientes["dni"])  # index busca la posicion en la lista
+    del pacientes[pos]   # borra al paciente, se va del hospital
 
 
-def DyC(pacientes):
-    if len(pacientes) == 1:   #caso base 1: hay un solo paciente
+def dyc(pacientes):
+    if len(pacientes) == 1:   # caso base 1: hay un solo paciente
         return pacientes
 
-    if len(pacientes) == 2:   #caso base 2: hay dos pacientes. ordenamos y retornamos la lista ordenada
-        if (pacientes.gravedad[0] > pacientes.gravedad[1]):
+    if len(pacientes) == 2:   # caso base 2: hay dos pacientes. ordenamos y retornamos la lista ordenada
+        if pacientes[0]["gravedad"] > pacientes[1]["gravedad"]:
           return pacientes
         else:
-          pacientes_aux= pacientes[0]
-          pacientes[0]=pacientes[1]
-          pacientes[1]=pacientes_aux
+          pacientes_aux = pacientes[0]
+          pacientes[0] = pacientes[1]
+          pacientes[1] = pacientes_aux
           return pacientes
 
 
     else:
         medio = len(pacientes) // 2
-        izq = DyC(pacientes[:medio])
-        der = DyC(pacientes[medio:])
-        pacientes = Ordenamiento(izq, der)
+        izq = dyc(pacientes[:medio])
+        der = dyc(pacientes[medio:])
+        pacientes = ordenamiento(izq, der)
 
     return pacientes      # ordenamos y devolvemos la lista de mayor a menor por merge sort
 
 
-def Ordenamiento(mitad1, mitad2):
+def ordenamiento(mitad1, mitad2):
     i, j = 0, 0
     lista_ordenada = []
 
-    while (i < len(mitad1) and j < len(mitad2)):
-        if (mitad1.gravedad[i] > mitad2.gravedad[j]):
+    while i < len(mitad1) and j < len(mitad2):
+        if mitad1[i]["gravedad"] > mitad2[j]["gravedad"]:
             lista_ordenada.append(mitad1[i])
             i += 1
         else:
             lista_ordenada.append(mitad2[j])
             j += 1
 
-    # Agregar lo que falta de una lista
+    # agregar lo que falta de una lista
     lista_ordenada += mitad1[i:]
     lista_ordenada += mitad2[j:]
 
